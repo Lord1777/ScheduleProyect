bbd0aae3d5ce92378c23f7424e925c1251d7e0e8
Samuel <rpolaniarubiano@gmail.com>
2023-11-16 16:46:52 -0500

    Creacion de proyecto laravel x react js

---
+++ Horario/resources/js/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../../css/Login.css";
+
+export const Login = () => {
+  return (
+    <>
+        <main className='fondo'>
+            <div className="formSignIn">
+                <form method='POST'>
+                    
+                </form>
+            </div>
+
+        </main>
+
+    </>
+  )
+}

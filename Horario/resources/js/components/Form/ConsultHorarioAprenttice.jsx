eb47450eabfe98cf66860cf307b893c92456d311
Ricardo <rpolaniarubiano@gmail.com>
2023-12-10 14:56:56 -0500

    Falta responsive de la interfaz consultar aprendiz

--- Horario/resources/js/components/Form/ConsultHorarioAprenttice.jsx
+++ Horario/resources/js/components/Form/ConsultHorarioAprenttice.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import '../../../css/Form/ConsultHorarioAprenttice.css'
 import logosena from '../../assets/img/LogoSena.png'
+import useDropdown from '../../hooks/useDropdown'
 
 export const ConsultHorarioAprenttice = () => {
+
+    const { isDropdown, selectedOption, handleDropdown, handleOptionClick} = useDropdown();
+
     return (
         <>
             <main className='background-consult-horario'>
@@ -10,22 +14,28 @@ export const ConsultHorarioAprenttice = () => {
                     <img src={logosena} alt="sena" />
                     <h2>Consultar Horario</h2>
                     <form method='POST'>
-                        
-                        <div className="select-ficha">
-                            <input type="text" 
-                            className='textBox'
-                            placeholder='Seleccionar Ficha'
+
+                        <div className={`select-ficha ${isDropdown ? 'open' : ''}`}>
+                            <input type="text"
+                                className='textBox'
+                                placeholder='Seleccionar Ficha'
+                                onClick={handleDropdown}
+                                value={selectedOption}
                             />
-                            <div className="options">
-                                
-                                <div>2560354 - Analis y desarrollo de software</div>
-                                <div>6821632 - Contabilidad</div>
-                                <div>2783190 - Gestión bancaria</div>
-                                <div>2132163 - Recursos Humanos</div>
-                                <div>2560354 - Analis y desarrollo de software</div>
-                                <div>6821632 - Contabilidad</div>
-                                <div>2783190 - Gestión bancaria</div>
-                                <div>2132163 - Recursos Humanos</div>
+                            <div className={`container-options ${isDropdown ? 'open' : ''}`}>
+                                <div className="container-search">
+                                    <input type="search" placeholder='Buscar' id='search' />
+                                </div>
+                                <div className="options">
+                                    <div className='option'>2560354 - Analis y desarrollo de software</div>
+                                    <div className='option'>6821632 - Contabilidad</div>
+                                    <div className='option'>2783190 - Gestión bancaria</div>
+                                    <div className='option'>2132163 - Recursos Humanos</div>
+                                    <div className='option'>2560354 - Analis y desarrollo de software</div>
+                                    <div className='option'>6821632 - Contabilidad</div>
+                                    <div className='option'>2783190 - Gestión bancaria</div>
+                                    <div className='option'>2132163 - Recursos Humanos</div>
+                                </div>
                             </div>
                         </div>
 

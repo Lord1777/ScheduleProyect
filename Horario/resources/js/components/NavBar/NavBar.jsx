c8a15004695e8406f37ed4c7539e1a5574a7fa44
Lord17777 <canegrn@misena.edu.co>
2023-12-25 17:27:41 -0500

    Ligero cambio en el Navbar

--- Horario/resources/js/components/NavBar/NavBar.jsx
+++ Horario/resources/js/components/NavBar/NavBar.jsx
@@ -25,8 +25,8 @@ export const NavBar = () => {
                         <img src={toggle} alt="toggle" />
                     </button>
                 </div>
-                <div>
-                    nombre
+                <div className='name-coordinator'>
+                    <h2>Nombre del Coordinador</h2>
                 </div>
             </nav>
             <div className={`options-nav ${isNavOpen ? 'open' : ''}`}>

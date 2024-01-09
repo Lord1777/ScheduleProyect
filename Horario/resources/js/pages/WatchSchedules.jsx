bebaeae8bb6e9e06e736282f65304f598956db95
Lord17777 <canegrn@misena.edu.co>
2024-01-09 17:24:43 -0500

    Perdonen la demora en terminar esto, pero ya casi termino

--- Horario/resources/js/pages/WatchSchedules.jsx
+++ Horario/resources/js/pages/WatchSchedules.jsx
@@ -4,64 +4,70 @@ import "../../css/WatchSchedules/WatchSchedules.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faCalendar } from '@fortawesome/free-solid-svg-icons';
 
-
-
 export const WatchSchedules = () => {
 
   return (
     <>
       <NavBar></NavBar>
       <div className="Space"></div>{/*Espacio creado para separar el contenido*/}
+      <div className="title-and-search">
+        <h2>Horarios Académicos</h2>
+        <div className="search-input">
+          <input
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Buscar"
+          />
+          <FontAwesomeIcon
+            icon={faSearch}
+            className="search-icon"
+          />
+        </div>
+      </div>{/*Titulo y buscador*/}
       <div className="main-container">
-        <div className="title-and-search">
-          <h2>Horarios Académicos</h2>
-          <div className="search-input">
-            <input
-              type="search"
-              name="search"
-              id="search"
-              placeholder="Buscar"
-            />
-            <FontAwesomeIcon
-              icon={faSearch}
-              className="search-icon"
-            />
+        <div className="schedule">
+          <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
+          <div className="ficha-and-number">
+            <h2>Ficha</h2>
+            <h2>2560354</h2>
           </div>
-        </div>{/*Titulo y buscador*/}
-
-
-        <div className="container-schedules">
-          <div className="schedule1">
-            <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
-            <div className="ficha-and-number">
-              <h2>Ficha</h2>
-              <h2>2560354</h2>
-            </div>
-          </div>{/*Contenedor de prueba*/}
-          <div className="schedule2">
-            <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
-            <div className="ficha-and-number">
-              <h2>Ficha</h2>
-              <h2>2560354</h2>
-            </div>
+        </div>{/*Contenedor de prueba*/}
+        <div className="schedule">
+          <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
+          <div className="ficha-and-number">
+            <h2>Ficha</h2>
+            <h2>2560354</h2>
           </div>
-          <div className="schedule3">
-            <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
-            <div className="ficha-and-number">
-              <h2>Ficha</h2>
-              <h2>2560354</h2>
-            </div>
+        </div>
+        <div className="schedule">
+          <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
+          <div className="ficha-and-number">
+            <h2>Ficha</h2>
+            <h2>2560354</h2>
           </div>
-          <div className="schedule4">
-            <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
-            <div className="ficha-and-number">
-              <h2>Ficha</h2>
-              <h2>2560354</h2>
-            </div>
+        </div>
+        <div className="schedule">
+          <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
+          <div className="ficha-and-number">
+            <h2>Ficha</h2>
+            <h2>2560354</h2>
           </div>
-        </div> {/*Contenedor de los horarios de la fichas*/}
-
-
+        </div>
+        <div className="schedule">
+          <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
+          <div className="ficha-and-number">
+            <h2>Ficha</h2>
+            <h2>2560354</h2>
+          </div>
+        </div>
+        <div className="schedule">
+          <FontAwesomeIcon icon={faCalendar} className="calendar-icon" />
+          <div className="ficha-and-number">
+            <h2>Ficha</h2>
+            <h2>2560354</h2>
+          </div>
+        </div>
       </div>{/*Contenedor principal*/}
     </>
   );

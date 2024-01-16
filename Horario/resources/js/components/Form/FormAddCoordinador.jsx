8564734e0ba72b0065d853aa2331aadaa1487260
SamuelPulgarin <samuelpulgarin37@gmail.com>
2024-01-16 13:37:45 -0500

    Interfaz de instructor y coordinador responsiva

--- Horario/resources/js/components/Form/FormAddCoordinador.jsx
+++ Horario/resources/js/components/Form/FormAddCoordinador.jsx
@@ -29,7 +29,7 @@ export const FormAddCoordinador = () => {
         idSede,
     });
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
         fetchSubmitCoordinator();
     }

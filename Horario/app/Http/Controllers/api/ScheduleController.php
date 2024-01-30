8532c8a8acc65dc9114e50a62a8e64a5fd087e69
SamuelPulgarin <samuelpulgarin37@gmail.com>
2024-01-30 16:33:02 -0500

    Horario academico: asignacion de horas y restriccion con limite de horas para instructores y ambientes

--- Horario/app/Http/Controllers/api/ScheduleController.php
+++ Horario/app/Http/Controllers/api/ScheduleController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Models\Ambiente;
 use App\Models\Asignacion;
 use App\Models\HorarioAcademico;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
+use App\Models\Usuario;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class ScheduleController extends Controller
@@ -30,6 +34,9 @@ class ScheduleController extends Controller
         }
 
         try {
+            //Inicio
+            DB::beginTransaction();
+
             $data = $request->json()->all();
             $idTrimestre = $data['idTrimestre'];
             $idFicha = $data['idFicha'];
@@ -44,7 +51,73 @@ class ScheduleController extends Controller
 
             $asignaciones = [];
 
+            $horasAntesInstructores = [];
+            $horasAntesAmbientes = [];
+
             foreach ($globalStoreBoxes as $box) {
+
+                $idInstructor = intval($box['idInstructor']);
+                $idAmbiente = intval($box['idAmbiente']);
+
+                // Obtener las horas antes solo si aún no se han obtenido antes para dicho [instructor]
+                if (!isset($horasAntesInstructores[$idInstructor])) {
+                    $horasAntesInstructores[$idInstructor] = Usuario::where('idUsuario', $idInstructor)->value('horasAsignadas');
+                }
+                // Obtener las horas antes solo si aún no se han obtenido antes para dicho [ambiente]
+                if (!isset($horasAntesAmbientes[$idAmbiente])) {
+                    $horasAntesAmbientes[$idAmbiente] = Ambiente::where('idAmbiente', $idAmbiente)->value('horasAsignadas');
+                }
+
+                $nombreInstructor = Usuario::where('idUsuario', $idInstructor)->value('nombreCompleto');
+                $numeroAmbiente = Ambiente::where('idAmbiente', $idAmbiente)->value('ambiente');
+
+                // Obtener el límite de horas para el instructor y ambiente
+                $limiteHorasInstructor = Usuario::where('idUsuario', $idInstructor)->value('limiteHoras');
+                $limiteHorasAmbiente = Ambiente::where('idAmbiente', $idAmbiente)->value('limiteHoras');
+
+                $disponibilidadInstructor = $limiteHorasInstructor - $horasAntesInstructores[$idInstructor];
+                $disponibilidadAmbiente = $limiteHorasAmbiente - $horasAntesAmbientes[$idAmbiente];
+
+                if ($limiteHorasInstructor !== null && $limiteHorasAmbiente !== null) {
+
+                    $nuevasHorasInstructor = Usuario::where('idUsuario', $idInstructor)->value('horasAsignadas') + 1;
+                    $nuevasHorasAmbiente = Ambiente::where('idAmbiente', $idAmbiente)->value('horasAsignadas') + 1;
+
+                    if ($nuevasHorasInstructor > $limiteHorasInstructor) {
+
+                        // Si el incremento supera el límite:
+                        DB::rollBack();
+
+                        return response()->json([
+                            'status' => 0,
+                            'message' => "El instructor '{$nombreInstructor}' ha alcanzado el límite de horas permitido: '{$limiteHorasInstructor} horas'. Por favor, revise la cantidad de veces que dicho instructor es asignado, considerando que dispone de '{$disponibilidadInstructor} horas' para impartir formación.",
+                            'error' => "The action cannot be performed. The limit of assigned hours is exceeded",
+                        ], Response::HTTP_BAD_REQUEST); //400
+                    }
+
+                    if ($nuevasHorasAmbiente > $limiteHorasAmbiente) {
+
+                        //Cancelar
+                        DB::rollBack();
+
+                        return response()->json([
+                            'status' => 0,
+                            'message' => "El ambiente '{$numeroAmbiente}' ha alcanzado el límite de horas permitido: '{$limiteHorasAmbiente} horas'. Por favor, revise la cantidad de veces que dicho ambiente es asignado, considerando que dispone de '{$disponibilidadAmbiente} horas'.",
+                            'error' => "The action cannot be performed. The limit of assigned hours is exceeded",
+                        ], Response::HTTP_BAD_REQUEST); //400
+                    }
+                }
+
+                Usuario::where('idUsuario', $idInstructor)->update([
+                    'horasAsignadas' => DB::raw('horasAsignadas + 1'),
+                    //Mas columnas...
+                ]);
+
+                Ambiente::where('idAmbiente', $idAmbiente)->update([
+                    'horasAsignadas' => DB::raw('horasAsignadas + 1'),
+                    //Mas columnas...
+                ]);
+
                 $asignaciones[] = [
                     'boxIndex' => $box['boxIndex'],
                     'idAmbiente' => intval($box['idAmbiente']),
@@ -56,13 +129,19 @@ class ScheduleController extends Controller
             // Insertar asignaciones en lote
             Asignacion::insert($asignaciones);
 
+            //Confirmar
+            DB::commit();
+
             return response()->json([
                 'status' => 1,
                 'message' => 'Successfully created academic schedule'
             ], Response::HTTP_CREATED); //201
         } catch (\Exception $e) {
+
+            //Cancelar
+            DB::rollBack();
             return response()->json([
-                'error' => "Register Schedule Error: ".$e->getMessage()
+                'error' => "Register Schedule Error: " . $e->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR); //500
         }
     }

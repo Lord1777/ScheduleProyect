fb33ec278133dc9b03b801013def80d578a12511
Lord1777 <canegrn@misena.edu.co>
2024-02-10 17:27:45 -0500

    Comenzando la lógica de sumar las horas asignadas a una ficha

---
+++ Horario/resources/js/context/FilterScheduleFichaContext.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useState } from 'react';
+//El que estoy desarrollando
+
+const FilterScheduleFichaContext = createContext();
+
+export const FilterScheduleFichaContextProvider = ({ children }) => {
+    const [idBoxIndex, setIdBoxIndex] = useState(null);
+    // const [idFicha, setIdFicha] = useState(null);
+  
+    const setIdBoxIndexValue = (boxIndex) => {
+        setIdBoxIndex(boxIndex);
+    };
+  
+    // const setIdFichaValue = (ficha) => {
+    //   setIdFicha(ficha);
+    // };
+  
+    return (
+      <FilterScheduleFichaContext.Provider value={{ idBoxIndex,setIdBoxIndexValue }}>
+        {children}
+      </FilterScheduleFichaContext.Provider>
+    );
+  };
+  
+  export default FilterScheduleFichaContext;

b25570dd9ee856fadcdd7e085fe92854f21b06e7
Lord1777 <canegrn@misena.edu.co>
2024-02-14 13:17:31 -0500

    Diseño responsive terminado del FormLogin

--- Horario/resources/js/components/Form/FormLogin.jsx
+++ Horario/resources/js/components/Form/FormLogin.jsx
@@ -28,9 +28,6 @@ export const FormLogin = () => {
         await authUser(data.documento, password);
     };
 
-    if (loading) {
-        //return <Loading/>
-    }
 
     return (
         <>
@@ -40,10 +37,7 @@ export const FormLogin = () => {
                         <img src={LogoSena} alt="LogoSena" />
                         <h2 className='name-proyect'><span>Advanced Schedule Planning Software</span></h2>
                     </div>
-
-                    <h2>Ingreso Usuarios Registrados
-                    </h2>
-
+                    <h2>Ingreso Usuarios Registrados</h2>
                     <form method="POST" onSubmit={handleSubmit(onSubmit)}>
                         <div className='grid-column'>
                             <div className={`dropdown ${isDropdown ? 'open' : ''}`}>

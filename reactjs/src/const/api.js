e43707a697a5bf16661e714650cc76b5272ca29b
Samuel <samuelpulgarin37@gmail.com>
2024-03-04 17:43:41 -0500

    intentando

--- reactjs/src/const/api.js
+++ reactjs/src/const/api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://sphapp.sweetmym.com/api';
+export const API_URL = 'https://sphapp.sweetmym.com/backend/api';
 export const API_KEY = '';
 export const csrf_token = /* getCookieCsrf(); */ '';
 

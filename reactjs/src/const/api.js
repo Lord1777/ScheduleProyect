7434c71610d80229a196f6b55197e570694e9a2b
SamuelPulgarin <samuelpulgarin37@gmail.com>
2024-03-07 21:09:20 -0500

    Revirtiendo variables globales a localhost para desarrollar las nuevas correcciones

--- reactjs/src/const/api.js
+++ reactjs/src/const/api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://apihorarios.softwaresolutionsdev.com/api';
+export const API_URL = 'http://localhost:8000/api';
 export const API_KEY = '';
 export const csrf_token = /* getCookieCsrf(); */ '';
 
